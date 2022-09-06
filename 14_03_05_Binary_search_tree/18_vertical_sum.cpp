926fa9e3bc10e5713521411e6ed753390adc39a7
Yashwant Singh <yashwantsing333@gmail.com>
2022-09-06 20:24:19 +0530

    vertical sum new approach

--- 14_03_05_Binary_search_tree/18_vertical_sum.cpp
+++ 14_03_05_Binary_search_tree/18_vertical_sum.cpp
@@ -53,3 +53,106 @@ int main(){
     }
     return 0 ;
 }
+
+/*
+    1) Came with this approach during revision
+
+    // Online C++ compiler to run C++ program online
+#include <iostream>
+#include <vector>
+#include<queue>
+#include<map>
+using namespace std;
+struct Node{
+    int key ;
+    Node *left,*right ;
+    int hd ;
+    Node (int key){
+        this->key = key ;
+        left = right = NULL ;
+        hd = 0 ;
+    }
+     
+} ;
+
+
+void print(Node *root){
+    if(root==NULL){
+        return ;
+    }
+    print(root->left) ;
+    cout<<root->hd<<" " ;
+    print(root->right) ;
+}
+
+
+void hd(Node *root,Node *parent){
+    if(root==NULL){
+        return ;
+    }
+    if(parent!=root&&parent->left==root){
+        root->hd = parent->hd-1 ;
+    }
+    if(parent!=root&&parent->right==root){
+        root->hd = parent->hd+1 ;
+    }
+    hd(root->left,root) ;
+    hd(root->right,root) ;
+}
+
+void verticalSum(Node *root,map<int,int>&m){
+    if(root==NULL){
+        return ;
+    }
+    queue<Node*>q ;
+    q.push(root) ;
+    while(q.empty()==false){
+        int count = q.size() ;
+        while(count>0){
+            auto temp = q.front() ;
+            m[temp->hd]+=temp->key ;
+            if(temp->left!=NULL){
+                q.push(temp->left) ;
+            }
+            if(temp->right!=NULL){
+                q.push(temp->right) ;
+            }
+            q.pop() ;
+            count-- ;
+        }
+        cout<<endl ;
+    }
+}
+
+
+int main() {
+   
+    Node *root = new Node(10) ;
+    root->left =  new Node(15) ;
+    root->left->left =  new Node(35) ;
+    root->left->left->left =  new Node(40) ;
+    root->left->right = new Node(20) ;
+    root->left->right->right = new Node(75) ;
+    root->left->right->right->right = new Node(80) ;
+    root->right = new Node(25) ;
+    // root->left->right =  new Node(15) ;
+    // root->right = new Node(30) ;
+    // root->right->left = new Node(11) ;
+    // root->right->right = new Node(30);
+    // root->right->right->left = new Node(25)
+    // ;
+    hd(root,root) ;
+    map<int,int>m ;
+    verticalSum(root,m) ;
+    for(auto it:m){
+        cout<<it.second<<" " ;
+    }
+    // print(root) ;
+    return 0;
+}
+    
+    
+    
+    
+    
+*/ 
