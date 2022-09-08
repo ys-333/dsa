d01bdd5548a2afc56254168b4748f92b8f8acd35
Yashwant Singh <yashwantsing333@gmail.com>
2022-09-08 20:01:10 +0530

    complete binary tree correction

--- 14_03_05_Binary_search_tree/22_completeTree.cpp
+++ 14_03_05_Binary_search_tree/22_completeTree.cpp
@@ -60,3 +60,81 @@ int main(){
     cout<<isComplete(root) ;
     return 0 ;
 }
+
+/*
+    In this method we store all node in vector no matter that is node with
+    real value or null value,
+    Then we traverse the node and see that if our previous was NULL and
+    our current is a vaid root, then we return false as it is not a complete 
+    binary tree.
+
+    #include <iostream>
+    #include <vector>
+    #include<queue>
+    #include<algorithm>
+    #include<map>
+    #include<set>
+    using namespace std;
+    struct Node{
+        int key ;
+        Node *left,*right ;
+        int hd ;
+        Node (int key){
+            this->key = key ;
+            left = right = NULL ;
+            hd = 0 ;
+        }
+        
+    } ;
+
+    vector<Node*>v;
+
+    void completeBinary(Node *root){
+        if(root==NULL){
+            return ;
+        }
+        queue<Node*>q ;
+                v.push_back(root) ;
+        q.push(root) ;
+        while(q.empty()==false){
+            int count = q.size() ;
+            while(count>0){
+                auto temp = q.front() ;
+                v.push_back(temp->left) ;
+                v.push_back(temp->right) ;
+                
+                if(temp->left!=NULL){
+                    q.push(temp->left) ;
+                }
+                if(temp->right!=NULL){
+                    q.push(temp->right) ;
+                }
+                q.pop() ;
+                count-- ;
+            }
+        }
+    }
+        
+
+    int main(){
+    Node *root = new Node(1) ;
+    root->left = new Node(2) ;
+    root->right= new Node(3) ;
+    root->left->left = new Node(4) ;
+    //   root->right->left = new Node(4) ;
+    //   root->right->right= new Node(5) ;
+    //   root->left->right = new Node(5) ;
+    completeBinary(root);
+    Node *prev = new Node(32132) ;
+    for(int i=0;i<v.size();i++){
+        
+        if(v[i]==NULL){
+            prev =NULL ;
+        }
+        else if(prev==NULL&&v[i]!=NULL){
+            cout<<"not a complete binary tree "<<endl ;
+            break ;
+        }
+    }
+    }
+*/ 
